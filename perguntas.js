1432d2bfea18be639fa9d86bbbf453f8d59bb031
sarasouo <159158955+sarasouo@users.noreply.github.com>
2024-11-15 00:30:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Mágica',
+    'Twilight Sparkle',
+    'Ela era uma Pônei unicórnio fêmea que se transforma em uma Alicórnio e se torna uma princesa em A Cura do Mistério Mágico. '
+)
+
+criaCartao(
+    'Honestidade',
+    'Applejack',
+    'Ela vive e trabalha no Rancho Maçã Doce com a sua avó paterna Vovó Smith, seu irmão mais velho Big McIntosh, sua irmã mais nova Apple Bloom e sua cadela Winona.'
+)
+
+criaCartao(
+    'Bondade',
+    'Fluttershy',
+    'Ela mora em uma pequena casa perto da Floresta da Liberdade e ela é uma zeladora de Animais, o mais proeminente de seus encargos é seu mascote Angel o coelho.'
+)
+
+criaCartao(
+    'Generosidade',
+    'Rarity',
+    'Rarity trabalha tanto como uma designer de moda como uma costureira em sua própria loja em Ponyville, a Boutique Carrossel. '
+)
+criaCartao(
+    'Alegria',
+    'Pinkie pie',
+    'Ela trabalha na Esquina do Torrão de Açúcar e vive no segundo andar da confeitaria com Gummy seu crocodilo bebê de estimação banguela.'
+)
+criaCartao(
+    'Lealdade',
+    'Rainbow Dash',
+    'Ela é responsável por controlar o clima e limpar os céus de Ponyville, Em Arco-Íris Supersônico, Rarity e a Princesa Celestia declaram que ela é a melhor voadora de toda Equestria'
+)
